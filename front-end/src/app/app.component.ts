c097370c9bd3ae25c4e801abf8b69ea059d2dc5e
Luca Vezzoli <luca@implast.it>
2021-08-14 14:24:46 +0200

    Fill env with config.json values

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Configs } from './base/models/configs.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('./assets/config.json').subscribe((data: Configs) => {
+      if (data) {
+        environment.title = data.title;
+        environment.basePathToTemplates = data.basePathToTemplates;
+        environment.categoriesToPrint = data.categoriesToPrint;
+      }
+    });
+  }
+}
