f1d98fd1aff9e1b0996aa615838fa623b4cda4d0
Luca Vezzoli <luca@implast.it>
2021-08-21 13:34:09 +0200

    Fix BE port & optionalLayer

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   path: 'http://127.0.0.1', // Connection to locale
-  port: 4203,
-  optionalLayer: '',
+  port: 3000,
+  optionalLayer: '/api',
 
   // path: 'http://192.168.1.8',    // Connection to LAN server
   // path: 'http://wrk.applehome.it',  // Connection to WAN server
   // port: undefined,
-  // optionalLayer: '/api',
 
   currency: '€',
 
