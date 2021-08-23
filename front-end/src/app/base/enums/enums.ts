ddfff61e4173fdaac9c2d235af7112ab3841c120
Luca Vezzoli <luca@implast.it>
2021-08-23 16:54:46 +0200

    Add routes

--- front-end/src/app/base/enums/enums.ts
+++ front-end/src/app/base/enums/enums.ts
@@ -4,8 +4,11 @@ export enum Urls {
   CASSA_STANDARD = 'cassa/standard',
   CASSA_BAR = 'cassa/bar',
   CASSA_ASPORTO = 'cassa/asporto',
+  AMMINISTRAZIONE_GESTIONE_UTENTI = 'amministrazione/gestione-utenti',
 }
 
 export enum ApiUrls {
   LOGIN = '/usr/login',
+  CASH_DESK = '/storage/',
+  USERS = '/usr/'
 }
