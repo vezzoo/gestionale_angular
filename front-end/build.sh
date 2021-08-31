896d74a8a24be16c34aaa55db5b4ce02c3478400
Luca <luca@implast.it>
2021-08-31 20:47:04 +0200

    Fixes

--- front-end/build.sh
+++ front-end/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "[34mRemoving files...[0m"
-rm -R ~/gestionale_angular/dist/*
+rm -R ~/gestionale/gestionale_angular/front-end/dist/*
 rm /var/www/gestionale_angular/html/*
 echo ""
 
@@ -16,13 +16,7 @@ echo "[34mBuilding...[0m"
 ng build --prod
 
 echo "[34mCoping files...[0m"
-cp -r ~/gestionale_angular/dist/gestionale_angular/* /var/www/gestionale_angular/html/
-echo ""
-
-echo "[34mRestart BE...[0m"
-echo "TODO"
-# ~/gestionale_angular/backend/scripts/stopServer.sh
-# ~/gestionale_angular/backend/scripts/startServer.sh
+cp -r ~/gestionale/gestionale_angular/front-end/dist/gestionale_angular/* /var/www/gestionale_angular/html/
 echo ""
 
 echo "[34mRestart nginx...[0m"
