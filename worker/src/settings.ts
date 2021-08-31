0f2c81fa58ce22cce51a30cfab742887c0fdfe7a
Stefano Fontana <stefano.fontana.2000@gmail.com>
2021-08-31 21:44:53 +0200

    changed settings

--- worker/src/settings.ts
+++ worker/src/settings.ts
@@ -4,16 +4,17 @@ export const SETTING_JWT_PUBLIC = "-----BEGIN PUBLIC KEY-----\n" +
     "-----END PUBLIC KEY-----\n"
 
 export const SETTING_API_ROOT = "/api"
-export const SETTING_WEBSERVER_HTTP_PORT = 4000
+export const SETTING_WEBSERVER_HTTP_PORT = 3001
 export const SETTING_WEBSERVER_BIND = "0.0.0.0"
 
 export const SETTING_PID_FILE = "./pid"
 
 export const SETTING_CORS_ORIGIN = "*"
 
+export const SETTING_CUPS_SERVER = "127.0.0.1"
 export const SETTING_PRINTERS = [
     {
         "name": "cucina1",
-        "ip": "127.0.0.1"
+        "printer": "casa"
     }
 ]
