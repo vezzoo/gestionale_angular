20202886dd6a9f6815891f12c08ef82ad1093d9a
Luca Vezzoli <luca@implast.it>
2021-09-03 21:53:44 +0200

    HOTFIX BE

--- backend/src/webserver/apicalls/AuthApiCall.ts
+++ backend/src/webserver/apicalls/AuthApiCall.ts
@@ -87,10 +87,11 @@ export default class AuthApiCall extends AbstractCall {
         if(user.permissions.includes("root")) return true
 
         for(let p of perms){
-            if(user.permissions.includes(p))
+            if(user.permissions.includes(p)) {
                 if(ored) return true
-            else
+            } else {
                 if (!ored) return false
+            }
         }
 
         return !ored
