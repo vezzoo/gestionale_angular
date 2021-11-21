9d8a353a795fa81eee46bf81f86af340a8b214f1
Luca Vezzoli <luca@implast.it>
2021-11-21 18:31:02 +0100

    Fix build process

--- front-end/build.sh
+++ front-end/build.sh
@@ -1,25 +1,27 @@
-#!/bin/sh
+# #!/bin/sh
 
-echo "[34mRemoving files...[0m"
-rm -R ~/gestionale/gestionale_angular/front-end/dist/*
-rm /var/www/gestionale_angular/html/*
-echo ""
+# echo "[34mRemoving files...[0m"
+# rm -R ~/gestionale/gestionale_angular/front-end/dist/*
+# rm /var/www/gestionale_angular/*
+# echo ""
 
-echo "[34mPull from git...[0m"
-git pull origin main
-echo ""
+# echo "[34mPull from git...[0m"
+# git pull origin main
+# echo ""
 
-echo "[34mInstall dependecies...[0m"
-npm i
+# echo "[34mInstall dependecies...[0m"
+# npm i
 
-echo "[34mBuilding...[0m"
-ng build --prod
+# echo "[34mBuilding...[0m"
+# ng build --prod
 
-echo "[34mCoping files...[0m"
-cp -r ~/gestionale/gestionale_angular/front-end/dist/gestionale_angular/* /var/www/gestionale_angular/html/
-echo ""
+# echo "[34mCoping files...[0m"
+# cp -r ~/gestionale/gestionale_angular/front-end/dist/gestionale_angular/* /var/www/gestionale_angular/html/
+# echo ""
 
-echo "[34mRestart nginx...[0m"
-sudo systemctl restart nginx
-sudo systemctl status nginx
-echo ""
+# echo "[34mRestart nginx...[0m"
+# sudo systemctl restart nginx
+# sudo systemctl status nginx
+# echo ""
+
+ts-node build.ts ~/gestionale/configs
