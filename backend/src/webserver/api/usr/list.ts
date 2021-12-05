5eed255478c35f6550e9138178c549f094aa061b
Luca Vezzoli <luca@implast.it>
2021-12-05 18:19:40 +0100

    HOTFIX isLeftHanded backend

--- backend/src/webserver/api/usr/list.ts
+++ backend/src/webserver/api/usr/list.ts
@@ -9,7 +9,7 @@ export default new AuthApiCall(
     async (req, res, user, _, __, parameters) => {
         let user_list = undefined
         if(user.has_permission("user_management"))
-            user_list = (await User.find(!!parameters.userid ? {_id: parameters.userid} : {}) as User[]).map(e => ({id: e._id?.toString() || "nope", username: e.username, permissions: e.permissions})) //all users
+            user_list = (await User.find(!!parameters.userid ? {_id: parameters.userid} : {}) as User[]).map(e => ({id: e._id?.toString() || "nope", username: e.username, permissions: e.permissions, isLefthanded: e.is_lefthanded})) //all users
 
         return {
             me: {id: user._id?.toString(), username: user.username, permissions: user.permissions},
